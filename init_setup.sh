a47bf45fe1d8979a6c31a8a700cf65c58743d014
zer0trac3 <zer0trac3@gmail.com>
2023-04-19 21:48:01 -0400

    Initial Setup

---
+++ init_setup.sh
@@ -0,0 +1,4 @@
+. /Users/zer0trace/anaconda3/bin/activate && conda activate /Users/zer0trace/anaconda3; 
+conda create --prefix ./env python=3.7 -y
+source activate ./env
+pip install -r requirements.txt
